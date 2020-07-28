28783a1b8f9784f67cb73cc283a76492d2c81eec
Clement Habinshuti <clhabins@microsoft.com>
2020-07-28 13:35:50 +0300

    Copied EdmObjectsGenerator code from ApiAsAService project

---
+++ DummyTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EdmObjectsGenerator;
+
+namespace DummyTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consle.WriteLine("Hello, World");
+        }
+    }
+}
